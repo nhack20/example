ad0ccfc8cb657a1f6360da3768add9f5803777bf
nursultanaitkulov <aitkulovnursultan.1809@gmail.com>
2022-09-28 09:24:02 +0600

    example commit

--- src/Solutions.java
+++ src/Solutions.java
@@ -6,25 +6,36 @@ public class Solutions {
     public static void main(String[] args) {
         List<Integer> list=new ArrayList<>();
         int res = 0;
-        
+        list.add(4);
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(1);
+
+        for(int i=0;i<list.size();i++){
+        }
+        for (int lists:list){
+
+
+        }
 
 
     }
 
-    public int singleNumber() {
-
-    }
+//    public int singleNumber() {
+//
+//    }
 //        Arrays.sort(nums);
 //
 //        for (int i = 0; i < nums.length - 1; i++) {///
 //            if (nums[i] == nums[i + 1]) {
-//                nums[i] = 0;
-//                nums[i + 1] = 0;
+//                nums[i] = -1;
+//                nums[i + 1] = -1;
 //            }
 //        }
-//        for (int num : nums) {
-//            if (num != 0) {
-//                return num;
+//        for (int numbers : nums) {
+//            if (numbers != -1) {
+//                return numbers;
 //            }
 //        }
 //        return 0;
